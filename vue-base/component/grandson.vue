a0f40200ff0dd61b88e0ba6a7190407c222596d9
zhouatie <1114349984@qq.com>
2019-10-20 14:58:08 +0800

    feat: component

---
+++ vue-base/component/grandson.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>grandson</div>
+</template>
+<script>
+export default {
+  props: ['b'],
+  mounted() {
+    console.log(this.$attrs, 'grandson')
+  }
+}
+</script>
+<style lang="less">
+</style>
