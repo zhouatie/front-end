7ff099435911a99dabb68b301de88aa312f1f761
zhouatie <1114349984@qq.com>
2019-11-21 23:21:30 +0800

    practise pm2

---
+++ practise/practise-pm2/index.js
@@ -0,0 +1,23 @@
+const app = require('express')()
+
+app.get('*', (req, res) => {
+  console.log(req.query)
+  console.log(req.header)
+  console.log(Object.keys(req))
+  res.json({
+    code: 0,
+    message: 'success'
+  })
+})
+app.post('*', (req, res) => {
+  console.log(req.body)
+  console.log(req.params, 'params')
+  res.json({
+    code: 0,
+    message: 'success'
+  })
+})
+
+app.listen(9000, () => {
+  console.log('启动成功！')
+})
---
+++ practise/practise-pm2/pm2.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [{
+    name: "ap",
+    script: "./index.js",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }]
+}
