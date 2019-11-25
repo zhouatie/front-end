d90257bc1fa1c24ea78cc6a96a1bc43715a2a3b4
zhouatie <1114349984@qq.com>
2019-11-26 01:04:01 +0800

    feat: about fs

--- practise-node/fs.js
+++ practise-node/fs.js
@@ -1,14 +1,33 @@
 const fs = require('fs')
 const path = require('path')
-const obj = fs.stat(path.resolve(__dirname, 'http.js'), (err, statObj) => {
-  if (err) {
-    return console.log(err)
-  }
-  console.log(Object.keys(statObj))
-  for (var i in statObj) {
-    console.log(i)
-  }
-  console.log(statObj.isFile(), 'isFile')
-  console.log(statObj.isDirectory(), 'isDirectory')
-  console.log(statObj.isBlockDevice(), 'isBlockDevice')
-})
+
+// fs.stat(path.resolve(__dirname, 'http.js'), (err, statObj) => {
+//   if (err) {
+//     return console.log(err)
+//   }
+//   console.log(Object.keys(statObj))
+//   for (var i in statObj) {
+//     console.log(i)
+//   }
+//   console.log(statObj.isFile(), 'isFile')
+//   console.log(statObj.isDirectory(), 'isDirectory')
+//   console.log(statObj.isBlockDevice(), 'isBlockDevice')
+// })
+
+// fs.readFile(path.resolve(__dirname, './test.json'), 'utf-8', (err, buffer) => {
+//   if (err) {
+//     return console.log(err)
+//   }
+//   console.log(Buffer.from(buffer).toString())
+//   fs.writeFile(path.resolve(__dirname, './data.txt'), buffer, (a, b) => {
+//     console.log(a, b, 'ab')
+//   })
+// })
+
+// fs.mkdir('./b/c', { recursive: true }, (err) => {
+//   console.log(err, 'err')
+// })
+
+// fs.rmdir('./a', { recursive: true }, (err) => {
+//   console.log(err, 'err')
+// })
